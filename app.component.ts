7f63a460abfbf939041c8f00ce524b2f31aec9d0
Andrey <65175184+DoroshukA@users.noreply.github.com>
2022-02-16 11:10:44 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app1';
+
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
+  fileChangeEvent(event: any): void {
+      this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+      this.croppedImage = event.base64;
+      localStorage.setItem(this.croppedImage, 'true');
+      console.log(this.croppedImage);
+  }
+  imageLoaded() {
+  }
+  cropperReady() {
+  }
+  loadImageFailed() {
+  }
+}
